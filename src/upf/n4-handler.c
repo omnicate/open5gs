83aebc124d419327c781fb10e7af6680f16315ee
Sukchan Lee <acetcom@gmail.com>
2020-08-07 21:48:17 -0400

    update it

--- src/upf/n4-handler.c
+++ src/upf/n4-handler.c
@@ -492,7 +492,7 @@ void upf_n4_handle_session_establishment_request(
     /* Send Buffered Packet to gNB/SGW */
     ogs_list_for_each(&sess->pfcp.pdr_list, pdr) {
         if (pdr->src_if == OGS_PFCP_INTERFACE_CORE) { /* Downlink */
-            upf_gtp_send_buffered_packet(pdr);
+            ogs_pfcp_send_buffered_packet(pdr);
         }
     }
 
@@ -611,7 +611,7 @@ void upf_n4_handle_session_modification_request(
     /* Send Buffered Packet to gNB/SGW */
     ogs_list_for_each(&sess->pfcp.pdr_list, pdr) {
         if (pdr->src_if == OGS_PFCP_INTERFACE_CORE) { /* Downlink */
-            upf_gtp_send_buffered_packet(pdr);
+            ogs_pfcp_send_buffered_packet(pdr);
         }
     }
 
